9aaeb10dc5ad5031335c9828a300feb1c0a5db26
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-03 10:41:31 +0300

    spring-07-SpringBoot-Demo

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,45 @@
+package com.cydeo.service;
+
+import com.cydeo.config.AppConfigData;
+import com.cydeo.config.DBConfigData;
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+    private final AppConfigData appConfigData;
+    private final DBConfigData dbConfigData;
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData, DBConfigData dbConfigData) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+        this.appConfigData = appConfigData;
+        this.dbConfigData = dbConfigData;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+
+    public void printConfigData() {
+        System.out.println(appConfigData.getUserName());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
+
+    }
+
+    public void printDbConfigData(){
+        System.out.println(dbConfigData.getUsername());
+        System.out.println(dbConfigData.getPassword());
+        System.out.println(dbConfigData.getType());
+    }
+}
