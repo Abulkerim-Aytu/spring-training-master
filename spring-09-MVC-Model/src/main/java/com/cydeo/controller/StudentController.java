44974a32ab47d0bd454fda8d05b0f79dc7926661
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-08 17:24:46 +0300

    spring-09-MVC-Model project updated

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,14 +1,17 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
 
-    @RequestMapping()
-    public String homePage(){
-
+    @RequestMapping("/welcome")
+    public String homePage(Model model) {
+        model.addAttribute("name", "Cydeo");
+        model.addAttribute("course", "MVC");
+        return "student/welcome";
     }
 
 }
