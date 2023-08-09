e1d072c41900132f51de7d3e7035bd7a9c188781
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-09 13:52:09 +0300

    spring-09-MVC-Model project updated 1

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,8 +1,14 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -11,6 +17,26 @@ public class StudentController {
     public String homePage(Model model) {
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
+
+        String subject = "Spring Boot";
+        model.addAttribute("subject", subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("id", studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        model.addAttribute("numbers", numbers);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date", dt);
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
+
         return "student/welcome";
     }
 
