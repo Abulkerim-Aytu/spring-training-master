74012b8268238c534e2f44b2a11fbdd82434e3bd
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-17 13:25:57 +0300

    spring-10-Thymeleaf updated 10

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/student")
 public class StudentController {
 
-//    @RequestMapping(value = "/register",method = RequestMethod.GET) //localhost:8080/student/register
+    //    @RequestMapping(value = "/register",method = RequestMethod.GET) //localhost:8080/student/register
     @GetMapping("/register")
     public String register(Model model){
 
@@ -19,9 +19,11 @@ public class StudentController {
 
     }
 
-    @RequestMapping(value = "/welcome") //localhost:8080/student/welcome?name=Ozzy
+//    We use GetMapping and PostMapping in the Method level
+
+    @RequestMapping(value = "/welcome",method = RequestMethod.POST) //localhost:8080/student/welcome?name=Ozzy
     @PostMapping("/welcome")
-    public String welcome(){
+    public String welcome(@RequestParam String name){
 
         return "student/welcome";
     }
