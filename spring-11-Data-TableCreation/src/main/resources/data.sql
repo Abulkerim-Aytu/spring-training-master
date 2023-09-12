16ca956aa7198deef86341c19f8e0d816b90a45d
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-12 17:31:09 +0300

    Spring Data update 3

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES(1,'Mike');
 
+INSERT INTO student(id,first_name,last_name,email)
+VALUES(1,'Mike','Smith','mike@cydeo.com');
+
+INSERT INTO student(id,first_name,last_name,email)
+VALUES(2,'Tom','Hanks','tom@cydeo.com');
