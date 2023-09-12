af9dbf94581b17b92bab1bdaf92121390157c621
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-12 18:14:03 +0300

    Spring Data update 4

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,15 +1,31 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
+@Table(name="students")
 public class Student {
 
         @Id
+        @GeneratedValue(strategy= GenerationType.IDENTITY)
         private Long id;
+
+        @Column(name= "studentFirstName")
         private String firstName;
+        @Column(name= "studentLastName")
         private String lastName;
         private String email;
+
+        @Column(columnDefinition = "DATE")
+        private LocalDate birthDate;
+        @Column(columnDefinition = "TIME")
+        private LocalTime birthTime;
+        @Column(columnDefinition = "TIMESTAMP")
+        private LocalDateTime birthDateTime;
 
 }
