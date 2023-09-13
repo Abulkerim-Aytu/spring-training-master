831a91f29ba7c155ec1abcb3115bc2b072bc31f0
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-13 11:25:56 +0300

    spring-12-Data-Relationship Update 2

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
@@ -0,0 +1,32 @@
+package com.cydeo.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "PaymentDetails")
+@NoArgsConstructor
+@Data
+public class PaymentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal merchantPaymentAmount;
+    private BigDecimal commissionAmount;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate payoutDate;
+
+    public PaymentDetail(BigDecimal merchantPaymentAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
+        this.merchantPaymentAmount = merchantPaymentAmount;
+        this.commissionAmount = commissionAmount;
+        this.payoutDate = payoutDate;
+    }
+}
