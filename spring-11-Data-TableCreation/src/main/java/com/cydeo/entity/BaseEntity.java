5cfd2f4767e39f762a2d5b9c2da3e3dc8b698449
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-13 10:34:42 +0300

    Spring Data update 8

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/BaseEntity.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,17 +1,14 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
-@Entity
-public class Departments {
+@MappedSuperclass
+public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String department;
-    private String division;
-
+    private Integer id;
 }
--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -1,12 +1,26 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
-public class Employee {
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee extends BaseEntity {
 
-    @Id
-    private int id;
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private int salary;
+
 }
