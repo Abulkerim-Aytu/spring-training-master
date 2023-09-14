5a831565b5074b7c9bd7a68afd52bd62b8b95445
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-14 12:54:30 +0300

    spring-13-Cinema-lab Update 2

--- spring-13-Cinema-lab/src/main/java/com/cydeo/entity/Cinema.java
+++ spring-13-Cinema-lab/src/main/java/com/cydeo/entity/Cinema.java
@@ -1,4 +1,27 @@
 package com.cydeo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Cinema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+    private String sponsoredName;
+
+    @ManyToOne
+    private Location location;
+
+    public Cinema(String name, String sponsoredName) {
+        this.name = name;
+        this.sponsoredName = sponsoredName;
+    }
 }
--- spring-13-Cinema-lab/src/main/java/com/cydeo/entity/Location.java
+++ spring-13-Cinema-lab/src/main/java/com/cydeo/entity/Location.java
@@ -1,4 +1,38 @@
 package com.cydeo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+    private String latitude;
+    private String longitude;
+    private String country;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String address;
+
+    public Location(String name, String latitude, String longitude, String country, String city, String state, String postalCode, String address) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.country = country;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.address = address;
+    }
+
 }
