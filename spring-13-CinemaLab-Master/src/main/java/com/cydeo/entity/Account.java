4ce39ec24f35b1bda8632861e6165af529c92a27
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-19 13:41:15 +0300

    spring-13-Cinema-lab Master

---
+++ spring-13-CinemaLab-Master/src/main/java/com/cydeo/entity/Account.java
@@ -0,0 +1,26 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.UserRole;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "account_details")
+public class Account extends BaseEntity {
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+    @OneToOne(mappedBy = "account")
+    private User user;
+}
