2757422271e6975f9e9060132803fbcf076dd0eb
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-19 18:30:28 +0300

    spring-14-Data-Queries Update 2

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+
+import com.cydeo.repository.RegionRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryDemo implements CommandLineRunner {
+
+    private final RegionRepository regionRepository;
+
+
+    public QueryDemo(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("-----------REGIONS-----------");
+
+        System.out.println("findByCountry:" + regionRepository.findByCountry("Canada"));
+
+       }
+}
---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -0,0 +1,12 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RegionRepository extends JpaRepository<Region,Long> {
+ //   Display all regions in Canada
+
+    List<Region> findByCountry(String country);
+}
