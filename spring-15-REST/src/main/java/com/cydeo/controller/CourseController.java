c86b991e9ea2473feafd37ff1885ceb7c46be52c
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-28 17:43:19 +0300

    spring-15-REST-update 6

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -43,8 +43,11 @@ public class CourseController {
     @PutMapping("id")
     public void updateCourse(@PathVariable("id") long courseId, @RequestBody CourseDTO course){
         courseService.updateCourse(courseId,course);
-
     }
 
+    @DeleteMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") long courseId){
+        courseService.deleteCourseById(courseId);
+    }
 
 }
