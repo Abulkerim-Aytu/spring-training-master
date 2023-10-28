cf9d8e3aa0b830d5f481be02d1657792ebfbceae
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-28 16:27:56 +0300

    spring-15-REST-update 1

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -2,13 +2,30 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController //@Controller + @ResponseBody
+@RestController //@Controller + @ResponseBody // with this Annotation MVC' VIEW is are out of box, we use API instead of view in here.
 @RequestMapping("/courses/api/v1")
 public class CourseController {
+
+    // what ever we put in to the controllers are DTO
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses(){
+
+        // return all courses
+        return courseService.getCourses();
+    }
+
+
 
 }
