de63ee400ceb827919fd03de986b84bc6ea0a119
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-28 16:14:42 +0300

    spring-15-REST- Begin

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,14 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //@Controller + @ResponseBody
+@RequestMapping("/courses/api/v1")
+public class CourseController {
+
+}
