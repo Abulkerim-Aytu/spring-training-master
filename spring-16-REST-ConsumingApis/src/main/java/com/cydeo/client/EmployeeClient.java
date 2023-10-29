be30afce4979da64def26e1e46b725e9cec6cacd
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-29 16:40:16 +0300

    spring-16-REST-ConsumingApis-Update 10

--- spring-16-REST-ConsumingApis/src/main/java/com/cydeo/client/EmployeeClient.java
+++ spring-16-REST-ConsumingApis/src/main/java/com/cydeo/client/EmployeeClient.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "https://dummyapi.io",name = "EMPLOYEE-CLIENT")
+@FeignClient(url="https://dummyapi.io",name = "EMPLOYEE-CLIENT")
 public interface EmployeeClient  {
 
     @GetMapping("/data/v1/user?limit=10")
-    Employee getEmployee(@RequestHeader("app-id") String id);
+    Employee getEmployee(@RequestHeader("app-id") String id); // RequestHeader = add header (key,value)
 }
