df72b88957dbe4f2de960d130c6e1b2d0d0ba186
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-29 11:02:42 +0300

    spring-16-REST-ConsumingApis Begin

---
+++ spring-16-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -0,0 +1,17 @@
+package com.cydeo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Spring17RestConsumingApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumingApisApplication.class, args);
+    }
+
+}
