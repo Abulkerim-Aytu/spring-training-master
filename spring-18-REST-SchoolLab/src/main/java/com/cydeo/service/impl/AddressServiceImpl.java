3bd040c7e93f37a3128c0cf6695f1b15d1490424
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-31 13:00:21 +0300

    spring-18-REST-SchoolLab-update 6

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
@@ -1,7 +1,5 @@
-/*
 package com.cydeo.service.impl;
 
-import com.cydeo.client.WeatherApiClient;
 import com.cydeo.dto.AddressDTO;
 import com.cydeo.dto.WeatherDTO;
 import com.cydeo.entity.Address;
@@ -20,15 +18,13 @@ public class AddressServiceImpl implements AddressService {
 
     private final AddressRepository addressRepository;
     private final MapperUtil mapperUtil;
-    private final WeatherApiClient weatherApiClient;
 
     @Value("${access_key}")
     private String access_key;
 
-    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil, WeatherApiClient weatherApiClient) {
+    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil) {
         this.addressRepository = addressRepository;
         this.mapperUtil = mapperUtil;
-        this.weatherApiClient = weatherApiClient;
     }
 
     @Override
@@ -46,7 +42,6 @@ public class AddressServiceImpl implements AddressService {
                 .orElseThrow(() -> new Exception("No Address Found!"));
 
         AddressDTO addressDTO = mapperUtil.convert(foundAddress, new AddressDTO());
-        addressDTO.setCurrentTemperature(getCurrentWeather(addressDTO.getCity()).getCurrent().getTemperature());
 
         return addressDTO;
 
@@ -63,7 +58,6 @@ public class AddressServiceImpl implements AddressService {
         addressRepository.save(addressToSave);
 
         AddressDTO updatedAddress = mapperUtil.convert(addressToSave, new AddressDTO());
-        updatedAddress.setCurrentTemperature(getCurrentWeather(updatedAddress.getCity()).getCurrent().getTemperature());
 
         return updatedAddress;
 
@@ -86,9 +80,6 @@ public class AddressServiceImpl implements AddressService {
 
     }
 
-    private WeatherDTO getCurrentWeather(String city) {
-        return weatherApiClient.getCurrentWeather(access_key, city);
-    }
+
 
 }
-*/
