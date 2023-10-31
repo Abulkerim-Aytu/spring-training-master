a2c0e2d353d4e35bcea9eab91d844ea74fc3b07f
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-31 12:41:16 +0300

    spring-18-REST-SchoolLab-update 3

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/Controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/Controller/SchoolController.java
@@ -1,7 +1,10 @@
 package com.cydeo.Controller;
 
+import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +14,21 @@ import java.util.List;
 public class SchoolController {
 
     private final TeacherService teacherService;
+    private final StudentService studentService;
 
-    public SchoolController(TeacherService teacherService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService) {
         this.teacherService = teacherService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/teachers")
     public List<TeacherDTO> readAllTeacher(){
         List<TeacherDTO> teachers = teacherService.findAll();
         return teachers;
+    }
+
+    @GetMapping("/students")
+    private ResponseEntity<ResponseWrapper> readAllStudents(){
+        return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved",studentService.findAll() ));
     }
 }
