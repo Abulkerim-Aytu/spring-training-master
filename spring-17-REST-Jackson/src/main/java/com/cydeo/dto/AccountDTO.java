328bce24e84a2a7b7420a8cce2c1c3032bb39fc3
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-31 11:13:45 +0300

    spring-17-REST-Jackson-update 2

--- spring-17-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-17-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -9,10 +9,11 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"address", "country"}, ignoreUnknown = true)
-@JsonIgnoreProperties(ignoreUnknown = true)
+// if I want to ignore multiple fields.
+@JsonIgnoreProperties(ignoreUnknown = true) // Unknown fields that are coming from outside are not getting to our app.(mostly unmatch fields)
 public class AccountDTO {
 
-    @JsonIgnore
+    @JsonIgnore // Ignore completely
     private String name;
     private String address;
     private String country;
@@ -24,5 +25,11 @@ public class AccountDTO {
     @JsonBackReference  // This field is not going to be serialized (we are not going to see this object to converted as json inside AccountDTO)
     // This annotation mean hide UserDTO inside the AccountDTO
     private UserDTO user;
+
+//    The difference between @JsonBackReference and @JsonIgnore:
+//    with jsonIgnore we can not send or change hided fields that annotated with @JsonIgnore
+//    with @JsonBackReference we can send or change hided fields.
+
+
 
 }
