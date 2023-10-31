91a037cdd09b44042e2b2097a0b9038dd4407b23
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-31 12:51:55 +0300

    spring-18-REST-SchoolLab-update 5

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/Controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/Controller/SchoolController.java
@@ -2,8 +2,10 @@ package com.cydeo.Controller;
 
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,12 @@ public class SchoolController {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
     }
 
     @GetMapping("/teachers")
@@ -30,5 +34,13 @@ public class SchoolController {
     @GetMapping("/students")
     private ResponseEntity<ResponseWrapper> readAllStudents(){
         return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved",studentService.findAll() ));
+    }
+
+    @GetMapping("/parents")
+    private ResponseEntity<ResponseWrapper> readAllParents(){
+    ResponseWrapper responseWrapper =
+            new ResponseWrapper(true,"Parents are retrieved successfully", HttpStatus.OK.value(),parentService.findAll());
+    return ResponseEntity.status(HttpStatus.OK)
+            .body(responseWrapper);
     }
 }
