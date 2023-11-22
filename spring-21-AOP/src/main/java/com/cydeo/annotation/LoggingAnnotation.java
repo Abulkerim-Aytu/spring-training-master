648ed17a0c209524b5931f7c49bd18328138281c
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-11-22 09:44:22 +0300

    spring-21-AOP update-2

--- spring-21-AOP/src/main/java/com/cydeo/annotation/LoggingAnnotation.java
+++ spring-21-AOP/src/main/java/com/cydeo/annotation/LoggingAnnotation.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD}) // In here we define, where we can use this annotation.
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoggingAnnotation {
 }
