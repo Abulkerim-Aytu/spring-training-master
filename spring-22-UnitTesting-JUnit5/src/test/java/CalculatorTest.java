9e794696103dc9c2a39b7cc73fa9d94b13141222
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-12-01 19:10:13 +0300

    spring-22-Unit-Testing update-3

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -7,31 +7,54 @@ class CalculatorTest {
     @Test
     void add(){
         System.out.println("Add methods");
+        int actual = Calculator.add(2,3);
+        assertEquals(5,actual,"Test Failed");
     }
 
     @Test
     void testCase1(){
         System.out.println("Test Case 1");
+        fail("Not implemented yet.");
     }
 
     @Test
     void testCase2(){
         System.out.println("Test Case 2");
+        assertTrue(Calculator.operator.equals("add"));
+//        assertEquals("add",Calculator.operator);
     }
 
     @Test
     void testCase3(){
         System.out.println("Test Case 3");
+        assertArrayEquals(new int[] {1,2,3}, new int[] {1,3,2} , "Arrays are not same.");
     }
 
     @Test
     void testCase4(){
         System.out.println("Test Case 4");
+
+        String nullString = null;
+        String notNullString = "Cydeo";
+
+        assertNull(nullString);
+        assertNotNull(notNullString);
+
+//        assertNull(notNullString);
+//        assertNotNull(nullString);
     }
 
     @Test
     void testCase5(){
         System.out.println("Test Case 5");
+
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+//        If we want to check object references wen can use this methods.
+        assertSame(c1,c2);
+        assertNotSame(c1,c3);
     }
 
 }
