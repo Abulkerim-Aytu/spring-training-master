2802ad8485e55522073c866ec4c75c6268c41183
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-12-01 17:50:37 +0300

    spring-22-Unit-Testing update-2

---
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add(){
+        System.out.println("Add methods");
+    }
+
+    @Test
+    void testCase1(){
+        System.out.println("Test Case 1");
+    }
+
+    @Test
+    void testCase2(){
+        System.out.println("Test Case 2");
+    }
+
+    @Test
+    void testCase3(){
+        System.out.println("Test Case 3");
+    }
+
+    @Test
+    void testCase4(){
+        System.out.println("Test Case 4");
+    }
+
+    @Test
+    void testCase5(){
+        System.out.println("Test Case 5");
+    }
+
+}
