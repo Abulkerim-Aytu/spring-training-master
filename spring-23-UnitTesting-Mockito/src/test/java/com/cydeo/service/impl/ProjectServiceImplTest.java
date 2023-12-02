2814a6aef73b1aea8fd72a666bddb3360c42385e
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-12-02 12:52:26 +0300

    spring-23-UnitTesting-Mockito update-3

---
+++ spring-23-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/ProjectServiceImplTest.java
@@ -0,0 +1,66 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.entity.Project;
+import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.repository.ProjectRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProjectServiceImplTest {
+
+    @Mock
+    ProjectRepository projectRepository;
+
+    @Mock
+    ProjectMapper projectMapper;
+
+    @InjectMocks
+    ProjectServiceImpl projectService;
+
+    @Test
+    void getByProjectCode_Test() {
+
+        // Given        --      Preparation of the test
+        when(projectRepository.findByProjectCode(anyString())).thenReturn(new Project());   // Stubbing
+        when(projectMapper.convertToDto(any(Project.class))).thenReturn(new ProjectDTO());
+
+        // When         --      Execution of the real method
+        ProjectDTO projectDTO = projectService.getByProjectCode(anyString());
+
+        // Then         --      Verification and assertion
+        InOrder inOrder = inOrder(projectRepository, projectMapper);  // I want to check the order of these 2 Mocks
+
+        inOrder.verify(projectRepository).findByProjectCode(anyString());   // We are providing in which order these 2 Mocks should be
+        inOrder.verify(projectMapper).convertToDto(any(Project.class));
+
+        assertNotNull(projectDTO);
+
+    }
+
+    @Test
+    void getByProjectCode_ExceptionTest() {
+
+        when(projectRepository.findByProjectCode("")).thenThrow(new NoSuchElementException("Project Not Found"));
+
+        Throwable throwable = assertThrows(NoSuchElementException.class, () -> projectService.getByProjectCode(""));
+
+        verify(projectRepository).findByProjectCode("");
+
+        verify(projectMapper, never()).convertToDto(any(Project.class));
+
+        assertEquals("Project Not Found", throwable.getMessage());
+
+    }
+
+}
